84097578bb0311a187388124bff2545a1deb1fe0
wdlea <wdleader18@gmail.com>
2022-07-23 20:35:26 +1200

    added data generation

---
+++ sample_data_generator.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import random
+
+DATA_FILE_PATH = "sample_usage_data.csv"
+
+sample_data = {}
+
+for i in range(0, 288):
+    sample_data[i] = {}
+    sample_data[i]["device"] = "AC"
+    sample_data[i]["action"] = random.choice(["On", "Off"])
+    sample_data[i]["mins"] = i * 5
+
+df = pd.DataFrame(data=sample_data)
+
+with open(DATA_FILE_PATH, "w") as f:
+    f.write(str(sample_data))
+
